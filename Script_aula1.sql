28619c90662e42855e84dead3098cca828e8d2d2
amandacoiado <135606303+amandacoiado@users.noreply.github.com>
2025-04-17 07:07:25 -0300

    Add files via upload

---
+++ Script_aula1.sql
@@ -0,0 +1,39 @@
+/*
+ SQL - Aula 01
+ 17/04/2025
+ */
+
+
+
+/* --------------------------------------------------------
+ * 1ª parte - Visualizar tabelas
+ * --------------------------------------------------------*/
+
+/* 1) Visualizar todo o conteúdo da tabela Cidades */
+
+
+
+
+/* 2) Visualizar todo o conteúdo da tabela Estados */
+
+
+
+
+/* 3) Visualizar os 100 primeiros registros da tabela Clientes */
+
+
+
+
+/* 4) Visualizar os campos cliente_id, nome, endereco e cidade_id da tabela Clientes */
+
+
+
+
+/* 5) Ordenar a tabela Clientes pelo Nome de forma crescente */
+
+
+
+
+
+/* 6) Ordenar a tabela Clientes pelo Nome de forma decrescente */
+
