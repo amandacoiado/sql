7b2b80cb0894ae4148e24f42dd51469a96e5b308
amandacoiado <135606303+amandacoiado@users.noreply.github.com>
2025-04-24 07:27:27 -0300

    Add files via upload

---
+++ Script_aula2.sql
@@ -0,0 +1,191 @@
+/*
+ SQL - Aula 01
+ 24/04/2025
+ */
+
+
+/* --------------------------------------------------------
+ * 2ª parte - Agregações - Perguntando para os dados 
+ * --------------------------------------------------------*/
+
+
+/* 1) Quantas agências existem no Banco Ramos? */
+
+
+
+
+-- Resposta: 
+
+
+
+/* 2) Quantas contas existem no Banco Ramos? */
+
+
+
+
+-- Resposta: 
+
+
+
+/* 3) Quantas contas ativas existem no Banco Ramos? */
+
+
+
+
+-- Resposta: 
+
+
+
+/* 4) Exite algum número de conta repetido? */
+
+-- Resposta: 
+
+
+
+/* 5) Quantas agências possuem contas ativas?*/
+
+
+
+
+-- Resposta: 
+
+
+
+/* 6) Quantos clientes possuem cadastro no Banco Ramos? */
+
+
+
+
+-- Resposta: 
+
+
+
+/* 7) Quantos clientes possuem contas ativas? */
+
+
+
+
+
+-- Resposta: 
+
+/* 8) Quantos clientes possuem contas inativas? */
+
+
+
+
+
+-- Resposta: 
+
+/* 9) Quantos registros de transações há? */
+
+
+
+
+
+-- Resposta:
+
+
+
+/* 10) Quantas registros de transações há no ano de 2024? */
+
+-- usando a data completa
+
+
+
+
+
+-- usando BETWEEN
+
+
+
+
+
+-- usando a função YEAR()
+
+
+
+
+-- Contagem
+
+
+
+-- Resposta: 
+
+
+
+/* 11) Quantas transações ocorreram em janeiro de 2024? */
+-- usando year() e month()
+
+
+-- usando a concatenação de ano e mês
+
+
+
+
+
+
+-- Resposta: 
+
+
+
+/* 12) Qual é o valor mínimo, em reais, de transação registrado? */
+
+
+
+
+-- Resposta: R$ 
+
+
+
+/* 13) Qual é o valor máximo, em reais, de transação registrado? */
+
+
+
+
+-- Resposta: R$ 
+
+
+
+/* 14) Qual o código das transações do tipo Pix? */
+
+
+
+
+-- Resposta: 11
+
+
+
+/* 15) Qual o valor médio, em reais, das transações do tipo Pix? */
+
+
+-- Resposta: R$ 
+
+
+
+/* 16) Qual o valor médio, em reais, das transações do tipo depósito? */
+-- Identificar o(s) código(s) dos depósitos
+
+
+
+-- calcular a média
+
+
+
+
+-- Resposta: R$
+
+
+
+/* 17) Qual o total de tarifas que o Banco Ramos já cobrou? */
+-- Identificar o código das tarifas
+
+
+-- calcular o total
+
+
+
+
+-- Resposta: R$
+
+
+
